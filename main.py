255332c66f25bb26c359ca07a1821d4c78d1b434
mizow <mizoguchi@outward.jp>
2026-02-05 10:32:02 +0900

    ローカルサーバー修正

--- main.py
+++ main.py
@@ -142,4 +142,4 @@ app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
